642cad08e98ebb85f825053b386ace27b2256351
masssster <varellinogallan@gmail.com>
2023-03-14 13:18:59 +0700

    feat : update home ui

--- my_app/home.py
+++ my_app/home.py
@@ -5,9 +5,17 @@ def launch():
         st.header('Stock Predict')
         st.write('Stock Predict is experimental application to do stock price prediction.')
         st.write('Stock Predict will do stock price prediction based on best model from skripsi experiment.')
+    with st.container():
+        st.header('Neural prophet')
+        st.write("NeuralProphet bridges the gap between traditional time-series models and deep learning methods.")
+        st.write("It's based on PyTorch and can be installed using pip.")
         st.write('Neural prophet is the best model for stock price prediction based on skripsi experiment.')
-        st.write('Prophet is good enought model for stock price prediction based on skripsi experiment.')
         st.write('More information about neural prophet from https://neuralprophet.com/')
-        st.write('More information about  prophet from https://facebook.github.io/prophet/')
-
+    with st.container():
+        st.header('Prophet')
+        st.write("Prophet is a forecasting procedure implemented in  R and Python.")
+        st.write("It is fast and provides completely automated forecasts that can be tuned by hand by data scientists and analysts.")
+        st.write('prophet is the good enough model for stock price prediction based on skripsi experiment.')
+        st.write('More information about prophet from https://facebook.github.io/prophet/')
+        
     pass
